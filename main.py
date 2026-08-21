3a953390d1fb2826ea5bc131fe77b69d48ad4fb4
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-21 20:43:48 +0530

    Working api creating

---
+++ main.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
+
+@app.get("/about")
+def about():
+    return {"message": "This is my FastAPI project"}
+
+@app.get("/hello/{name}")
+def hello(name: str):
+    return {"message": f"Hello {name}"}
+
+class User(BaseModel):
+    name : str
+    age  : int
+    
+
+@app.post("/users")
+def create_user(user : User):
+    return {
+        "message" :  "User created successfully",
+        "user" : user
+    }

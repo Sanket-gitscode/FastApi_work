9b84be4d7066824708c3c74cce1fa9a11c37f476
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-23 00:58:01 +0530

    added all main CRUD requirments and tested them

--- expense_tracker_api/main.py
+++ expense_tracker_api/main.py
@@ -25,7 +25,7 @@ def create_expense(expense : Expense):
     
     new_expense = expense.model_dump()
     
-    new_expense['id'] = len(expenses) + 1
+    new_expense["id"] = max([expense["id"] for expense in expenses], default=0) + 1
     
     expenses.append(new_expense)
     
@@ -38,3 +38,24 @@ def get_expense(expense_id: int):
             return expense
 
     raise HTTPException(status_code=404, detail='Expense not found')
+
+@app.delete("/expenses/{expense_id}")
+def delete_expense(expense_id: int):
+    for expense in expenses:
+        if expense["id"] == expense_id:
+            expenses.remove(expense)
+            return {"message": "Expense deleted successfully"}
+
+    raise HTTPException(status_code=404, detail="Expense not found")
+
+@app.put("/expenses/{expense_id}")
+def update_expense(expense_id: int, updated_expense: Expense):
+    for expense in expenses:
+        if expense["id"] == expense_id:
+            expense["title"] = updated_expense.title
+            expense["amount"] = updated_expense.amount
+            expense["category"] = updated_expense.category
+
+            return expense
+
+    raise HTTPException(status_code=404, detail="Expense not found")

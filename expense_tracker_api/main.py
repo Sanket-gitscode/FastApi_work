c865137ae6210534084711d93fa2e83f334e697e
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-22 23:33:28 +0530

    added route and more REST style design feature -Phase1

--- expense_tracker_api/main.py
+++ expense_tracker_api/main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
+from pydantic import BaseModel
+from data import expenses
 
 app = FastAPI()
+
+
+class Expense(BaseModel):
+    title: str
+    amount: float
+    category: str
 
 
 @app.get("/")
 def home():
     return {"message": "Expense Tracker API is running!"}
+
+
+@app.get("/expenses")
+def get_expenses():
+    return expenses
+
+@app.post("/expenses")
+def create_expense(expense : Expense):
+    
+    new_expense = expense.model_dump()
+    
+    new_expense['id'] = len(expenses) + 1
+    
+    expenses.append(new_expense)
+    
+    return new_expense
+
+@app.get("/expenses/{expense_id}")
+def get_expense(expense_id: int):
+    for expense in expenses:
+        if expense["id"] == expense_id:
+            return expense
+
+    raise HTTPException(status_code=404, detail='Expense not found')

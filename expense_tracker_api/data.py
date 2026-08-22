ab32321350a2132f4743a631e2fa19dc38cf3986
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-22 20:23:08 +0530

    added data file and started working on expense tracker

---
+++ expense_tracker_api/data.py
@@ -0,0 +1,15 @@
+expenses = [
+    {
+        "id": 1,
+        "title": "Lunch",
+        "amount": 250,
+        "category": "food"
+    },
+    {
+        "id": 2,
+        "title": "Uber",
+        "amount": 180,
+        "category": "transport"
+    }
+]
+

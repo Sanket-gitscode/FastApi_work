8cc72a3c4adbd2b4d631dc6b7ca230df6528a47f
Sanket-gitscode <sanketyt11@gmail.com>
2026-08-29 14:27:06 +0530

    pydantic model test

---
+++ test_api/pydantic_test.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class Expense(BaseModel):
+    title: str
+    amount: float
+    category: str
+
+
+expense = Expense(
+    title="Movie",
+    amount=150,
+    category="entertainment"
+)
+
+print(expense)
+print(type(expense))
+
+print(expense.title)
+print(expense.amount)
+print(expense.category)
+
+print(expense.model_dump())
+print(type(expense.model_dump()))
+
